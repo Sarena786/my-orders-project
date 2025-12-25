e211662ecbbc98783c6fb719c4ee56cf9066f083
sarena <sareena.aul@student.mahidol.edu>
2025-12-25 10:43:50 +0700

    add passEncoder in backendAPP

--- pos-backend/src/main/java/com/sarena/posbackend/PosBackendApplication.java
+++ pos-backend/src/main/java/com/sarena/posbackend/PosBackendApplication.java
@@ -3,6 +3,9 @@ package com.sarena.posbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class PosBackendApplication {
 
@@ -10,4 +13,8 @@ public class PosBackendApplication {
         SpringApplication.run(PosBackendApplication.class, args);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
