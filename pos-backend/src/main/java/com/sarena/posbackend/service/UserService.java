37c0002bd8844d01ab3764dc1380a18b768bd91c
sarena <sareena.aul@student.mahidol.edu>
2025-12-25 10:38:37 +0700

    update UserService

--- pos-backend/src/main/java/com/sarena/posbackend/service/UserService.java
+++ pos-backend/src/main/java/com/sarena/posbackend/service/UserService.java
@@ -1,4 +1,22 @@
 package com.sarena.posbackend.service;
 
+import com.sarena.posbackend.entity.User;
+import com.sarena.posbackend.repository.UserRepository;
+
+import java.util.Optional;
+
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
 }
