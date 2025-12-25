51118c78d8ec59c223e89ec1ed9eae999fc63ce3
sarena <sareena.aul@student.mahidol.edu>
2025-12-25 10:31:06 +0700

    create a user table + userRepo (interface)

--- pos-backend/src/main/java/com/sarena/posbackend/entity/User.java
+++ pos-backend/src/main/java/com/sarena/posbackend/entity/User.java
@@ -3,15 +3,55 @@ package com.sarena.posbackend.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "users")
+@Table(
+        name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username")
+        }
+)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "user_id")
+    private Long userId;
 
-    private String name;
-    private String email;
+    @Column(nullable = false, length = 50)
+    private String username;
 
-    // getters & setters
+    @Column(nullable = false)
+    private String password;
+
+    // ===== Constructors =====
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // ===== Getters & Setters =====
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
