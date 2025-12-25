0bacc4fba4ce9a71f4b8d9aee9600e5ae14915c7
sarena <sareena.aul@student.mahidol.edu>
2025-12-25 11:13:39 +0700

    implement register and login in UserController

---
+++ pos-backend/src/main/java/com/sarena/posbackend/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.sarena.posbackend.controller;
+
+import com.sarena.posbackend.entity.User;
+import com.sarena.posbackend.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // register
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        try {
+            User savedUser = userService.register(user.getUsername(), user.getPassword());
+            return ResponseEntity.ok(savedUser);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    // login
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        try {
+            boolean success = userService.login(user.getUsername(), user.getPassword());
+            if(success)
+        }
+
+    }
+
+}
