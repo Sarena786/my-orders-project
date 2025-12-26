c22c4ece482af86eca876face75e271cd394d73d
sarena <sareena.aul@student.mahidol.edu>
2025-12-26 17:27:24 +0700

    update JWT and calling in frontend

--- pos-backend/src/main/java/com/sarena/posbackend/config/SecurityConfig.java
+++ pos-backend/src/main/java/com/sarena/posbackend/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.sarena.posbackend.config;
 
+import com.sarena.posbackend.security.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
@@ -14,13 +16,13 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/api/users/register",
-                                "/api/users/login"
-                        ).permitAll()
+                        .requestMatchers("/api/users/register", "/api/users/login").permitAll()
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        new JwtFilter(),
+                        UsernamePasswordAuthenticationFilter.class
                 );
-
         return http.build();
     }
 }
--- pos-backend/src/main/java/com/sarena/posbackend/security/JwtFilter.java
+++ pos-backend/src/main/java/com/sarena/posbackend/security/JwtFilter.java
@@ -1,4 +1,32 @@
 package com.sarena.posbackend.security;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws java.io.IOException, jakarta.servlet.ServletException {
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+
+            try {
+                JwtUtil.validateTokenAndGetUsername(token);
+            } catch (Exception e) {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
