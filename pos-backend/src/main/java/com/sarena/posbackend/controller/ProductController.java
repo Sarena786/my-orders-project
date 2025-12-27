e3ef9d9ab2c613c35e979cb336621d2eb54cf627
sarena <sareena.aul@student.mahidol.edu>
2025-12-27 17:25:44 +0700

    add product controller

---
+++ pos-backend/src/main/java/com/sarena/posbackend/controller/ProductController.java
@@ -0,0 +1,41 @@
+package com.sarena.posbackend.controller;
+
+import com.sarena.posbackend.entity.Product;
+import com.sarena.posbackend.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAll() {
+        return productService.findAll();
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product update(
+            @PathVariable Long id,
+            @RequestBody Product product
+    ) {
+        return productService.update(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        productService.delete(id);
+    }
+}
