99a2d4034152779d568ed34be671f7193cb93465
sarena <sareena.aul@student.mahidol.edu>
2025-12-27 22:08:15 +0700

    remove content type from product.js when getting request (allowing to upload file)

--- frontend/src/services/product.js
+++ frontend/src/services/product.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8080/api/products",
-    headers: {
-        "Content-Type": "application/json"
-    }
+    baseURL: "http://localhost:8080/api/products"
 });
 
 // JWT is already attached by interceptor globally
