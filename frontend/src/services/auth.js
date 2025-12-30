72cab2a239329fc3becb1c600cdd420dedf44c19
sarena <sareena.aul@student.mahidol.edu>
2025-12-30 13:17:10 +0700

    creating new api.js to call API and be interceptor

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,25 +1,9 @@
-import axios from "axios";
-
-const API = axios.create({
-    baseURL: "http://localhost:8080/api/users",
-    headers: {
-        "Content-Type": "application/json"
-    }
-});
-
-API.interceptors.request.use(config => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
+import API from "./api";
 
 export function login(username, password) {
-    return API.post("/login", { username, password });
+    return API.post("/users/login", { username, password });
 }
 
 export function register(username, password) {
-    return API.post("/register", { username, password });
+    return API.post("/users/register", { username, password });
 }
--- frontend/src/services/product.js
+++ frontend/src/services/product.js
@@ -1,32 +1,16 @@
-import axios from "axios";
-
-axios.interceptors.request.use(config => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
-const API = axios.create({
-    baseURL: "http://localhost:8080/api/products"
-});
-
-// JWT is already attached by interceptor globally
+import API from "./api";
 
 export function getProducts() {
-    return API.get("");
+    return API.get("/products");
 }
 
 export function addProduct(form) {
     const formData = new FormData();
-
     formData.append("name", form.name);
     formData.append("pricePerKg", form.pricePerKg);
     formData.append("quantity", form.quantity);
     formData.append("note", form.note || "");
-
-    return API.post("", formData);
+    return API.post("/products", formData);
 }
 
 export function updateProduct(id, data) {
